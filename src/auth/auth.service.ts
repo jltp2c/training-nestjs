3bc864b73dc64a5e12c328318d841ecc978b35eb
jltp2c <jeanluc.tan75@gmail.com>
2024-10-11 19:37:02 +0200

    Push nestjs starter

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signup() {
+    return { msg: 'i have signup' };
+  }
+
+  signin() {
+    return { msg: 'i have signin' };
+  }
+}
